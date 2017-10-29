87d3ab09a0c6e711e6037f8060a61bd4af77d521
Rabin Gaire <rabingaire20@gmail.com>
2017-10-29 14:53:19 +0545

    initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
+import { Header } from './src/components/common/';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Header headerText="Tech Stack" />
+        </View>
+      </Provider>
+    )
+  }
+}
