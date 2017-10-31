7209aaacbcd9290508e820dfff0940e85ce7a63b
Rabin Gaire <rabingaire20@gmail.com>
2017-10-31 14:27:25 +0545

    bunch of list can be seen on the screen

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, ListView } from 'react-native';
 import { connect } from 'react-redux';
 
+import ListItem from './ListItem';
+
 class LibraryList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.renderRow = this.renderRow.bind(this);
+    }
+
+    componentWillMount() {
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.dataSource =  ds.cloneWithRows(this.props.libraries);
+    }
+
+    renderRow(library) {
+        return (
+            <ListItem library={library}/>
+        )
+    }
+
     render() {
         return (
-            <View />
+            <ListView
+                dataSource={this.dataSource}
+                renderRow={this.renderRow}
+            />
         )
     }
 }
